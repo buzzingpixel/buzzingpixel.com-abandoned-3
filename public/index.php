1e7b9e70ad0689495995c5d3295c6a7efee718ed
TJ Draper <tj@buzzingpixel.com>
2019-04-22 23:38:10 -0500

    Install HTTP and CLI kernels and set up front controllers

--- public/index.php
+++ public/index.php
@@ -2,4 +2,15 @@
 
 declare(strict_types=1);
 
-echo 'hello world';
+use corbomite\di\Di;
+use corbomite\http\Kernel;
+
+require dirname(__DIR__) . '/config/bootstrap.php';
+
+if (getenv('DEV_MODE') === 'true') {
+    require dirname(__DIR__) . '/config/devMode.php';
+}
+
+/** @noinspection PhpUnhandledExceptionInspection */
+// TODO: Add error handler argument for 404 and 500 error pages in production
+Di::diContainer()->get(Kernel::class)();
