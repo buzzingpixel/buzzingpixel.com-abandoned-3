f15bd9bc2a2b8bb20f1dab374532003fd8565ebf
TJ Draper <tj@buzzingpixel.com>
2019-05-15 22:11:35 -0500

    Finish out testing for ScribbleApi

--- tests/Common/Infrastructure/FileContent/ScribbleApiTest.php
+++ tests/Common/Infrastructure/FileContent/ScribbleApiTest.php
@@ -7,6 +7,8 @@ namespace Tests\Common\Infrastructure\FileContent;
 use App\Common\Infrastructure\FileContent\ScribbleApi;
 use BuzzingPixel\Scribble\ScribbleApi as ScribbleApiSource;
 use BuzzingPixel\Scribble\Services\GetContentFromFile\GetContentFromFileDelegate;
+use BuzzingPixel\Scribble\Services\GetContentFromPath\GetContentFromPathDelegate;
+use BuzzingPixel\Scribble\Services\GetContentPathCollection\GetContentPathCollectionDelegate;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Throwable;
@@ -50,12 +52,56 @@ class ScribbleApiTest extends TestCase
         $this->scribbleApiSource->expects(self::once())
             ->method('getContentFromFile')
             ->with(
-                self::equalTo(
-                    $this->baseDir . 'testInputPath'
-                ),
+                self::equalTo($this->baseDir . 'testInputPath'),
                 self::equalTo($handler)
             );
 
         $this->scribbleApi->getContentFromFile('/testInputPath', $handler);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testGetContentFromPath() : void
+    {
+        /** @var MockObject&GetContentFromPathDelegate $handler */
+        $handler = $this->createMock(GetContentFromPathDelegate::class);
+
+        $this->scribbleApiSource->expects(self::once())
+            ->method('getContentFromPath')
+            ->with(
+                self::equalTo($this->baseDir . 'testInputPath'),
+                self::equalTo($handler),
+                self::equalTo(['md', 'txt'])
+            );
+
+        $this->scribbleApi->getContentFromPath(
+            '/testInputPath',
+            $handler,
+            ['md', 'txt']
+        );
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testGetContentPathCollection() : void
+    {
+        /** @var MockObject&GetContentPathCollectionDelegate $handler */
+        $handler = $this->createMock(GetContentPathCollectionDelegate::class);
+
+        $this->scribbleApiSource->expects(self::once())
+            ->method('getContentPathCollection')
+            ->with(
+                self::equalTo($this->baseDir . 'testInputPath'),
+                self::equalTo($handler),
+                self::equalTo(['md', 'txt'])
+            );
+
+        $this->scribbleApi->getContentPathCollection(
+            '/testInputPath',
+            $handler,
+            ['md', 'txt']
+        );
     }
 }
